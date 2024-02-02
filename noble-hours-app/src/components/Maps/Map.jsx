db10794d5c55e21cf4aa349b7c19976fd3dbe3df
Tommy Goodman <tommysdevelopment@gmail.com>
2024-02-03 00:34:38 +1300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   noble-hours-app/src/components/Maps/Map.jsx

--- noble-hours-app/src/components/Maps/Map.jsx
+++ noble-hours-app/src/components/Maps/Map.jsx
@@ -1,12 +1,12 @@
-const Map = (params) => {
-  return (
-    <iframe
-      src="https://storage.googleapis.com/maps-solutions-sd6x71vh8m/locator-plus/q25a/locator-plus.html"
-      width="100%"
-      height="100%"
-      style={{ border: "1" }}
-      loading="lazy"></iframe>
-  );
+const Map = (props) => {
+    return (
+        <iframe
+            src="https://storage.googleapis.com/maps-solutions-sd6x71vh8m/locator-plus/q25a/locator-plus.html"
+            width="100%"
+            height="100%"
+            style={{ border: "1" }}
+            loading="lazy"></iframe>
+    );
 };
 
 export default Map;
